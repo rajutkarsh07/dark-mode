d8a2ffc60d8a4dcea1b5fbfbc8d368f3d48b5934
utkarsh_raj <rajutkarsh2505@gmail.com>
2022-09-29 02:04:24 +0530

    added dark mode funcnality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import "./App.css";
-import Search from "./components/Search";
-import { useState } from "react";
+import Form from "./components/Form";
+import { createContext, useState } from "react";
+import ReactSwitch from "react-switch";
+
+export const ThemeContext = createContext(null);
 
 function App() {
-  const [inputvalue, setInputvalue] = useState("");
+  const [theme, setTheme] = useState("dark");
 
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
   return (
-    <div className="App">
-      <Search />
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme}>
+        <Form />
+        <div className="switch">
+          <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+        </div>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
